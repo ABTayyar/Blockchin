9a5aa47f38eddaa8a658b92772ef13f8dacc103f
ABTayyar <arht79@gmail.com>
2022-06-19 18:05:31 +0300

    feat v1.2

--- Block.js
+++ Block.js
@@ -2,50 +2,60 @@ import { sha256 } from "crypto.js";
 
 class Block{
 
-    constructor(hash,data,previoshash,height,nonce,timeStamps){
-        this.hash = this.mainig();
-        this.nonce = 0;
-        this.data = data;
+    //first we create counstrctor for a block, it takes:
+    //hash = fuction calculateHash() or fuction maining()
+    //nonce -> its for make a chance from 0 to number (for lop)
+    //data of block
+    //previoshash
+    //height -> for any block there is height
+    //timeStamps = Date.now() in sec:
+    
+    constructor(hash,data,previoshash,height,nonce,timeStamps,time){
+        this.hash = this.proofOfWork();
+        this.nonce =parseInt(this.proofOfWork().substring(77));
+        this.data = "temp";
         this.previoshash = previoshash;
         this.height = height;
-        this.timeStamps = timeStamps;
-        
+        this.timeStamps =parseInt(this.proofOfWork().substring(64,77));
+        // this.time = (new Date().getTime().toString()).slice(0,-3);
     }
 
-    
+    //Function calculateHash -> to make Hashes for our blocks
     calculateHash(){
         return sha256(this.height+this.previoshash+this.data+this.timeStamps).toString();
     }
 
-    mainig(){
-       let block = "Abdulrahman al tayyar"
-     
-       let nonce = 0;
-       let numberOfGuesses = 0;
+  
 
-       for (let i = 0; i < 10_000_000; i++) {
-        let magicHash = sha256(block + nonce + Date.now()).toString();
-        console.log(magicHash)
-        //5 it's mean to diffclute 
-        if (magicHash.substring(0, 5) =="0000") { 
-
-            console.log("magic hash : " + magicHash) 
-            console.log("we found the hash with "+numberOfGuesses+" guesses") 
-            return
-            
-        }
-        numberOfGuesses++;
+    proofOfWork(){
+        let data = "temp";
+        let nonce = 0;
+        let numOfGuesses = 0;
+        let difficulty = 1;
+        let timeStamp = Date.now()
+        
+        for (let i = 0; i < 10000000; i++){   
+        let magicHash = sha256(
+            data + nonce + timeStamp
+        ).toString();
+            if (magicHash.substring(0, difficulty) == "".padStart(difficulty, "0")){
+                console.log("magic hash : " + magicHash)
+                console.log("we found the hash with "+numOfGuesses+" guesses")
+                return magicHash + timeStamp + nonce;
+            }
+        numOfGuesses++; 
         nonce++;
-
-       }
-
-    }
+        }
+ 
 
 
 }
+}
+//اوبجكت
+var x=new Block();
+// console.log(x.time);
+// console.log(Date.now().toString())
 
-new Block();
 
-
-
+//لازم اسوي اكسبورت بدونه ماقدر اتعامل مع الكلاسات
 export default Block;
--- Blockchin.js
+++ Blockchin.js
@@ -1,8 +1,10 @@
-import Block from "./Block.mjs";
+import { BlockList } from "net";
+import Block from "./Block.js";
 
 
 class Blockchin{
 
+    
     
     chain;
     constructor(){
@@ -11,19 +13,63 @@ class Blockchin{
     }
 
     init(){
-        let block = new Block(0,"Genesis","null",0,0,Date.now());
-        if(this.chain.length!=0){
+
+        let block = new Block(0,"Genesis","null",0,0,Date.now(),0);
+        if(this.chain.length<=0){
             return;
         }
         this.chain.push(block);
         console.log(this.chain);
         
+        for(let i =1; i< 10 ; i ++){
+            let block = new Block("",data,this.chain[i-1].hash,this.chain.length,0,Date.now());
+
+            this.chain.push(block);
+
+            console.log(this.chain[i]);
+            
+                } 
     }
 
-  
+    // addBlock(data){
+    //    let block = new Block(0,data,this.chain[this.chain.length-1],this.chain.length,0,0,Date.now());
+    //    if(this.chain.length<=0){
+    //     return;
+    // }
+    // this.chain.push(block);
+    // console.log(this.chain);
+    // }
+
+   
+    validateBlocks(){
+        let bool = true;
+        for(let i = 1; i<this.chain.length; i++){
+            if(this.chain[i].prev != this.chain[i-1].hash)
+            bool = false;
+        }
+        console.log(bool);
+        
+        for(let i = 0; i<this.chain.length; i++){
+          if (!(sha256(this.chain[i].data + this.chain[i].nonce + this.chain[i].timeStamp).toString() == this.chain[i].hash)){
+            bool = false;
+          }
+
+         }
+            console.log(flag);
+        }
+
+ 
+
+
 }
-    
-new Blockchin();
+var x=new Blockchin();
+console.log("------------------------------------------------------------------------------------")
+
+
+
+
+
+
 
     
 export default Blockchin
